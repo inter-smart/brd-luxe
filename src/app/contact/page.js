8727c040cab798c74df6f9d24122cfecd1589004
ajayux <ajay@intersmart.in>
2025-08-07 10:25:57 +0530

    sell page

---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import InnerHero from "@/components/common/InnerHero";
+
+export default function Page() {
+  return (
+    <InnerHero
+      title="Contact"
+      mobileImage={"/images/banner-contact-1.jpg"}
+      desktopImage={"/images/banner-contact-1.jpg"}
+      alt="Contact Us Hero"
+    />
+  );
+}
--- src/components/common/InnerHero.jsx
+++ src/components/common/InnerHero.jsx
@@ -1,12 +1,6 @@
 import Image from "next/image";
 
-export default function InnerHero({
-  title,
-  desktopImage,
-  mobileImage,
-  alt,
-  className,
-}) {
+export default function InnerHero({ title, desktopImage, mobileImage, alt }) {
   return (
     <section className="w-full h-auto min-h-[376px] xl:min-h-[468px] 2xl:min-h-[576px] 3xl:min-h-[768px] relative z-0 flex items-end py-[30px] sm:py-[40px] xl:py-[50px] 2xl:py-[60px]">
       <picture className="w-full h-full absolute -z-1 inset-0">
@@ -20,7 +14,7 @@ export default function InnerHero({
           priority
         />
       </picture>
-      <div className="w-full h-full bg-gradient-to-t from-black to-transparent pointer-events-none absolute -z-1 top-0 left-0 bg-black/10" />
+      <div className="w-full h-full bg-gradient-to-t from-black via-40% via-transparent to-transparent pointer-events-none absolute -z-1 top-0 left-0 bg-[rgba(0,0,0,0.05)]" />
       <div className="container">
         <h1 className="text-[22px] sm:text-[26px] lg:text-[34px] xl:text-[48px] 2xl:text-[52px] 3xl:text-[64px] leading-none font-light font-base1 uppercase text-white">
           {title}
