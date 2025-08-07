e63f71034c84c7818f5c6031402a00cb7df618c9
abish <abish@intersmart.in>
2025-08-07 10:24:27 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Page() {
       <OfferSection /> 
       <BrandSection />
       <BrdAdvantageSection />
-      {/* <SellCarSection /> */}
+      <SellCarSection />
       <ExperienceSection />
       <LatestBrdSection />
       <JourneyFrameSection />
