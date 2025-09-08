d7695a873095366a56ab26a3d215859a2dab71ea
Sonu <sonu@intersmart.com>
2025-09-08 15:34:07 +0530

    Changes new

--- src/app/news/[slug]/page.js
+++ src/app/news/[slug]/page.js
@@ -2,7 +2,11 @@ import InnerHero from "@/components/common/InnerHero";
 import NewsDetailSection from "@/components/features/news/NewsDetailSection";
 import RelatedSection from "@/components/features/news/RelatedSection";
 
-export default async function Page() {
+export default async function Page({ params }) {
+
+  // Get the slug from the URL params
+  const { slug } = await params;
+
   // Fetch news page data from WP API
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/wp-json/custom/v1/news`,
@@ -12,17 +16,27 @@ export default async function Page() {
     throw new Error("Failed to fetch News Page data");
   }
   const data = await res.json();
-  const post  = data.posts?.[0] || {};
+
+
+  // Find the post that matches the slug
+  const post = data.posts?.find((post) => post.slug === slug) || {};
+
+  // If no post is found, you can handle it (e.g., return a 404)
+  if (!post || Object.keys(post).length === 0) {
+    return <div>Post not found</div>; // Or use Next.js's notFound()
+  }
+
   const banner = post.news_detail_banners?.[0];
+
   return (
     <>
-    {banner?.enable__disable_news_detail_banner && (
-      <InnerHero
-        title={banner.news_detail_banner_title}
-        mobileImage={banner.news_detail_mobile_image?.url}
-        desktopImage={banner.news__detail_desktop_image?.url}
-        alt={banner.news__detail_desktop_image?.alt}
-      />
+      {banner?.enable__disable_news_detail_banner && (
+        <InnerHero
+          title={banner.news_detail_banner_title}
+          mobileImage={banner.news_detail_mobile_image?.url}
+          desktopImage={banner.news__detail_desktop_image?.url}
+          alt={banner.news__detail_desktop_image?.alt}
+        />
       )}
       <NewsDetailSection data={post} />
       {post.related_posts && post.related_posts.length > 0 && (
