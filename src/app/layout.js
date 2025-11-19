bbb94152494ec08c240d2a304179ebc309a17d96
ajayux <ajay@intersmart.in>
2025-11-19 14:49:46 +0530

    meta pixel code in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -128,6 +128,35 @@ export default async function RootLayout({ children }) {
       {process.env.NEXT_PUBLIC_GTAG_ID && (
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTAG_ID} />
       )}
+      <head>
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '833166408969081');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=833166408969081&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+
       <body
         className={`${cormorantGaramond.variable} ${raleway.variable} ${CeraPro.variable} bg-black antialiased min-h-screen flex flex-col`}
       >
